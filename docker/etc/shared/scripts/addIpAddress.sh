066d2fa93d88d9f89573300af52a31e2a0a5bbb2
Krzysztof Bielecki <kbielecki@netizens.pl>
2020-11-10 07:49:58 +0100

    Init

---
+++ docker/etc/shared/scripts/addIpAddress.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+ 
+#
+# Retrieve the argument and create a name for a tmp file
+#
+processfile=$1
+tmpfile=${1}.tmp
+  
+#
+# for all running docker containers
+#
+for service in `docker ps -q`; do
+   #
+   # Extract the servicename and ipaddress
+   #
+   servicename=`docker inspect --format '{{ .Name }}' $service `
+   ipaddress=`docker inspect --format '{{ .NetworkSettings.IPAddress }}' $service`
+  
+   #
+   # if there is a service name and ipaddress
+   #
+   if [ ! -z $ipaddress ] &&  [ ! -z $servicename ] ;
+   then
+        # get rid of the first character - this is '/'
+  
+        servicename=${servicename:1}
+  
+        # remove the service name from the process file, and add it again
+        #grep -v $servicename $processfile > $tmpfile
+        echo -e $ipaddress '\t' $servicename
+        #mv $tmpfile $processfile
+   fi
+done
